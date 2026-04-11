8d0152d9b3387ff1db1e1618d0d43786af45a04a
K. Szczepankiewicz <szczpankiewicz.k@gmail.com>
2026-04-11 09:27:49 +0200

    feat: add addBookmarkButtonForm listener

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const mainSection = document.getElementById('main-section');
 const formSection = document.getElementById('form-section');
 const bookmarkListSection = document.getElementById('bookrmark-list-section');
 
+const nameEl = document.getElementById('name');
+const url = document.getElementById('url');
+const categoryDropdown = document.getElementById('category-dropdown');
+
+const addBookmarkButton = document.getElementById('add-bookmark-button');
+const addBookmarkButtonForm = document.getElementById('add-bookmark-button-form');
+const closeFormButton = document.getElementById('close-form-button');
+
+const categoryNames = document.getElementsByClassName('category-name');
+
 const getBookmarks = () => {
     const raw = localStorage.getItem('bookmarks');
     if (!raw) return [];
@@ -32,4 +42,31 @@ const displayOrCloseForm = () => {
     mainSection.classList.toggle('hidden');
     formSection.classList.toggle('hidden');
 }
+
+addBookmarkButton.addEventListener('click', (e) => {
+    Array.from(categoryNames).forEach(el => el.textContent = categoryDropdown.value);
+    displayOrCloseForm();
+});
+
+closeFormButton.addEventListener('click', displayOrCloseForm);
+
+const setBookmarks = bookmarks => localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+addBookmarkButtonForm.addEventListener('click', (e) => {
+    const bookmarks = getBookmarks();
+    bookmarks.push({ name: nameEl.value, url: url.value, category: categoryDropdown.value });
+    setBookmarks(bookmarks);
+    nameEl.value = '';
+    url.value = '';
+    displayOrCloseForm();
+});
+
 // const displayOrCloseForm = () => displaySection(mainSection.classList.contains('hidden') ? mainSection : formSection);
+// const checked = document.querySelector('input[type="radio"]:checked');
+
+
+
+
+
+
+
